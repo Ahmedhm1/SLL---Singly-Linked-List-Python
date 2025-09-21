fe209b278c1485f456d8646995bab19791cc5119
Ahmed Hisham <139140231+Ahmedhm1@users.noreply.github.com>
2025-09-21 22:54:41 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="linkedlist",
+    version="0.1.0",
+    author="Ahmedhm1",
+    description="A simple Singly Linked List implementation in Python",
+    license="MIT",
+    packages=find_packages(),
+)
